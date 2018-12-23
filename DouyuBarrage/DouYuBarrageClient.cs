70f17f20ba59b70f21cac9ba3c4afd33773a1940
supergcl <983944994@qq.com>
2018-12-23 23:01:04 +0800

    添加日志框架log4net

--- DouyuBarrage/DouYuBarrageClient.cs
+++ DouyuBarrage/DouYuBarrageClient.cs
@@ -63,7 +63,36 @@ namespace DouyuBarrage
         {
             if (tcpClient != null && tcpClient.Connected)
             {
+                // 发送登录请求
                 LoginRequest loginRequest = new LoginRequest(roomId);
+                BarragePacket sendPacket = new BarragePacket(loginRequest.ToString(), MessageType.CLIENT);
+                NetworkStream ns = tcpClient.GetStream();
+                ns.Write(sendPacket.Bytes, 0, sendPacket.Bytes.Length);
+                ns.Flush();
+
+                // 接收登录请求回应
+                byte[] recvBuffer = new byte[512];
+                int readCnt = ns.Read(recvBuffer, 0, 4); // 先读取包长度
+                if (readCnt != 4)
+                {
+                    throw new Exception("Read Error");
+                }
+
+                // 解析出包长度
+                int messageLength = BitConverter.ToInt32(recvBuffer);
+
+                // 根据读取到的包长，接收剩下的包体
+                recvBuffer = new byte[messageLength];
+                readCnt = ns.Read(recvBuffer, 0, messageLength);
+                if (readCnt != messageLength)
+                {
+                    throw new Exception("Read Error");
+                }
+
+                // 解析响应
+                BarragePacket recvPacket = new BarragePacket(recvBuffer);
+
+                // 记录日志
             }
 
             return false;
